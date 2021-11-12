a51318f700f15368048bcb16199ad6bb4fff2d55
jinicha <jinichaa@gmail.com>
2021-11-11 18:55:09 -0800

    Complete iss tracker practice

--- practice/issTracker.py
+++ practice/issTracker.py
@@ -1,1 +1,9 @@
+import requests
 
+data = requests.get(url="http://api.open-notify.org/iss-now.json")
+iss_postion = data.json()["iss_position"]
+latitude = iss_postion["latitude"]
+longitude = iss_postion["longitude"]
+current_location = (latitude, longitude)
+
+print(current_location)
